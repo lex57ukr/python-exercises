877b58095c376ae2541a86e6bc106e3e027125d8
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-07-24 14:03:54 +0300

    use a better name for slicing

--- series/series.py
+++ series/series.py
@@ -2,7 +2,7 @@ def slices(series, size):
     if not 1 <= size <= len(series):
         raise ValueError('Invalid size %r for %r' % (size, series))
 
-    def _slice_at(i):
+    def _cast_slice_at(i):
         return [int(n) for n in series[i: i+size]]
 
-    return [_slice_at(i) for i in range(len(series) - size + 1)]
+    return [_cast_slice_at(i) for i in range(len(series) - size + 1)]
