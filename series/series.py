5b18d8ca681001e61239813d4c5b81d75004f5df
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-07-24 13:40:34 +0300

    do the slicing and conversion in one go

--- series/series.py
+++ series/series.py
@@ -2,5 +2,7 @@ def slices(series, size):
     if not 1 <= size <= len(series):
         raise ValueError('Invalid size %r for %r' % (size, series))
 
-    numbers = [int(n) for n in series]
-    return [numbers[i: i+size] for i in range(len(numbers) - size + 1)]
+    def _slice_at(i):
+        return [int(n) for n in series[i: i+size]]
+
+    return [_slice_at(i) for i in range(len(series) - size + 1)]
