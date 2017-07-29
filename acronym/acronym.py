c2a6f96fc3c0328f250606ccbab5c893e6c89dd1
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-07-29 18:04:14 +0300

    use a shorter expression

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,4 +1,3 @@
 def abbreviate(text):
-    def _first_title_letters():
-        return [c for c in text.title() if c.isupper()]
-    return ''.join(_first_title_letters())
+    titles_only = (c for c in text.title() if c.isupper())
+    return ''.join(titles_only)
