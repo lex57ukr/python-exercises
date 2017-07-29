0dd9d11d0e180c880de5c0aae3c3974ab7839972
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-07-29 15:55:25 +0300

    make it more expressive

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,5 +1,6 @@
 def sum_of_multiples(number, factors):
-    def _is_mul(n):
-        return any((n for f in factors if n % f == 0))
-
-    return sum([n for n in range(1, number) if _is_mul(n)])
+    def _multiples():
+        def _is_mul(n):
+            return any((n % f == 0 for f in factors))
+        return [n for n in range(1, number) if _is_mul(n)]
+    return sum(_multiples())
