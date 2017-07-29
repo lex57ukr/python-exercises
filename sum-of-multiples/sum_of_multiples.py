46aee13866bcc019b8585cc7ed5a43ac9369de78
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-07-30 00:18:57 +0300

    use sets for faster math

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,6 +1,6 @@
 def sum_of_multiples(number, factors):
-    def _multiples():
-        def _is_mul(n):
-            return any((n % f == 0 for f in factors))
-        return [n for n in range(1, number) if _is_mul(n)]
-    return sum(_multiples())
+    multiples = set().union(*(
+        set(range(f, number, f)) for f in factors
+    ))
+
+    return sum(multiples)
