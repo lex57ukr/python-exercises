334cb1f4d707b20a4261900066f808e766f14660
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-03 22:18:36 +0300

    add largest_series_product exercise

---
+++ largest-series-product/largest_series_product.py
@@ -0,0 +1,13 @@
+from operator import mul
+from functools import reduce
+
+
+def largest_product(sequence, series_length):
+    if not 0 <= series_length <= len(sequence):
+        raise ValueError
+
+    def _groups():
+        for i in range(len(sequence) + 1 - series_length):
+            yield tuple(int(x) for x in sequence[i:i+series_length])
+
+    return max(reduce(mul, g, 1) for g in _groups())
