15ac89bb35d950420348f1f580f2f5c54dcf85c8
Alex Akhtyrskiy <lex57ukr@gmail.com>
2017-08-03 22:28:40 +0300

    fix formatting

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -8,6 +8,6 @@ def largest_product(sequence, series_length):
 
     def _groups():
         for i in range(len(sequence) + 1 - series_length):
-            yield tuple(int(x) for x in sequence[i:i+series_length])
+            yield tuple(int(x) for x in sequence[i: i+series_length])
 
     return max(reduce(mul, g, 1) for g in _groups())
